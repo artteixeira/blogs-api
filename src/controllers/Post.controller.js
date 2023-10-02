a1fc89e71992ed5a99dd7e598d757ae69aa8f790
Arthur Teixeira <arthursony1@hotmail.com>
2023-10-02 13:30:18 -0300

    req 14

--- src/controllers/Post.controller.js
+++ src/controllers/Post.controller.js
@@ -29,13 +29,26 @@ const createNewPost = async (req, res) => {
   return res.status(201).json(newPost);
 };
 
-const getAll = async (req, res) => {
+const getAll = async (_req, res) => {
   const posts = await PostService.getAll();
 
   return res.status(200).json(posts);
 };
 
+const getById = async (req, res) => {
+  const { id } = req.params;
+
+  const post = await PostService.getById(id);
+
+  if (!post) {
+    return res.status(404).json({ message: 'Post does not exist' });
+  }
+
+  return res.status(200).json(post);
+};
+
 module.exports = {
   createNewPost,
   getAll,
+  getById,
 };
--- src/routes/Post.routes.js
+++ src/routes/Post.routes.js
@@ -6,5 +6,6 @@ const verifyPostBlogPosts = require('../middlewares/verifyPostBlogPosts');
 
 route.post('/', validateJWT, verifyPostBlogPosts, PostController.createNewPost);
 route.get('/', validateJWT, PostController.getAll);
+route.get('/:id', validateJWT, PostController.getById);
 
 module.exports = route;
